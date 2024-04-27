8967c4fb8c7e098d7216a4658630b77a0de9b7c4
Ayush chalisee <ayushchalise12@gmail.com>
2024-04-27 11:58:43 +0200

    added static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,8 +54,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware'
 
 
-
-
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -124,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
